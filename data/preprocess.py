8d6907284e2feb30e362056ce36ef41d4ce296da
lucyz1125 <zhaols1125@outlook.com>
2025-10-23 15:18:34 +0800

    Create preprocess.py

---
+++ data/preprocess.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+
+def preprocess_data():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    return (x_train, y_train), (x_test, y_test)
